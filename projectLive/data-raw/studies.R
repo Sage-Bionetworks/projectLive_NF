90c388da336f3a522682e97551da883caa6d3087
andrewelamb <andrewelamb@gmail.com>
2020-07-27 13:44:56 -0700

    all raw data files now accomodate list annotation columns

--- projectLive/data-raw/studies.R
+++ projectLive/data-raw/studies.R
@@ -1,24 +1,20 @@
-## code to prepare `DATASET` dataset goes here
+# ## code to prepare `DATASET` dataset goes here
 
-library(synapser)
-library(purrr)
-library(glue)
-library(dplyr)
+source("get_synapse_tbl.R")
 
-synapser::synLogin()
+library(magrittr)
 
-#select columns from a synTable that are not STRING_LISTs
-synid <- "syn16787123"
-columns <- as.list(synapser::synGetTableColumns(glue::glue("{synid}")))
-select_cols <- columns %>% purrr::keep(function(x) !x$columnType == "STRING_LIST")
-select_colnames <- base::unlist(base::lapply(select_cols, '[[', "name"))
-studies <- synapser::synTableQuery(glue::glue("SELECT '{paste(select_colnames,collapse=\"','\")}' FROM {synid}"))$asDataFrame()
+# use your own condaenv here!!!!!
+reticulate::use_condaenv(
+  condaenv = "py37b",
+  required = TRUE,
+  conda = "/home/aelamb/anaconda3/condabin/conda"
+)
 
+synapseclient <- reticulate::import("synapseclient")
+syn <- synapseclient$Synapse()
+syn$login()
 
-# studies <- synapser::synTableQuery("SELECT 'studyName','studyId','studyFileviewId','studyStatus',
-#                                    'dataStatus', 'studyLeads', 'fundingAgency', 'summary', 'diseaseFocus', 
-#                                    'manifestation', 'consortium','id', 'accessRequirements', 'acknowledgementStatements',
-#                                    'relatedStudies', 'institutions' FROM syn16787123")$asDataFrame()
+studies <- get_synapse_tbl(syn, "syn16787123")
 
-#load("data-raw/pubs.RData")
 usethis::use_data(studies, overwrite = TRUE)
