83ab8d202b496022d4dd43fae682787644d7fcae
andrewelamb <andrewelamb@gmail.com>
2020-07-30 15:48:29 -0700

    one tab workling with data from synapse

--- projectLive/R/app_server.R
+++ projectLive/R/app_server.R
@@ -1,9 +1,9 @@
 #' @import shiny
 app_server <- function(input, output,session) {
   # List the first level callModules here
-  funding_partner <- callModule(mod_about_page_server, "about_page_ui_1")
-  callModule(mod_summary_snapshot_server, "summary_snapshot_ui_1", funding_partner)
-  callModule(mod_file_status_server, "file_status_ui_1", funding_partner)
-  callModule(mod_study_lead_server, "study_lead_ui_1", funding_partner)
-  callModule(mod_study_summary_server, "study_summary_ui_1", funding_partner)
+  funder_object <- callModule(mod_about_page_server, "about_page_ui_1")
+  callModule(mod_summary_snapshot_server, "summary_snapshot_ui_1", funder_object)
+  # callModule(mod_file_status_server, "file_status_ui_1", funder_object)
+  # callModule(mod_study_lead_server, "study_lead_ui_1", funder_object)
+  # callModule(mod_study_summary_server, "study_summary_ui_1", funder_object)
 }
