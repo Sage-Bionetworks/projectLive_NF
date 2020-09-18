3a37397d37826484b707ac199c7d42385d84e482
andrewelamb <andrewelamb@gmail.com>
2020-09-18 08:50:03 -0700

    fix files.r to only get files, and not folders

--- data-raw/files.R
+++ data-raw/files.R
@@ -4,6 +4,7 @@ syn <- create_synapse_login()
 files <- get_synapse_tbl(syn, "syn16858331")
 studies <- get_synapse_tbl(syn, "syn16787123")
 files <- files %>% 
+  dplyr::filter(.data$type == "file") %>% 
   dplyr::mutate(fundingAgency = studies$fundingAgency[match(projectId, studies$studyId)]) 
 
 saveRDS(files, "files.RDS")
