9f37d9e1cdcbb9cd8c3c5fd32c791d7e9156db57
Jineta Banerjee <jinetab@gmail.com>
2021-02-25 22:03:03 -0800

    Update files.R
    rename initiative column to consortium for v1.0

--- data-raw/files.R
+++ data-raw/files.R
@@ -3,7 +3,8 @@ devtools::load_all()
 syn <- create_synapse_login()
 files <- get_synapse_tbl(syn, "syn16858331")
 studies <- get_synapse_tbl(syn, "syn16787123")
-files <- files %>% 
+files <- files %>%
+  dplyr::rename("consortium" = "initiative") %>%
   dplyr::filter(.data$type == "file") %>% 
   dplyr::mutate(fundingAgency = studies$fundingAgency[match(projectId, studies$studyId)]) 
 
