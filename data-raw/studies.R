bd3eb7550ed51c06cf66ddc6639f3bd35074e0fd
Jineta Banerjee <jinetab@gmail.com>
2021-02-25 22:04:07 -0800

    Update studies.R

--- data-raw/studies.R
+++ data-raw/studies.R
@@ -1,7 +1,9 @@
 require(magrittr)
 devtools::load_all()
 syn <- create_synapse_login()
-studies <- get_synapse_tbl(syn, "syn16787123")
+studies <- get_synapse_tbl(syn, "syn16787123") %>%
+   dplyr::rename("initiative" = "consortium")
+
 saveRDS(studies, "studies.RDS")
 store_file_in_synapse(
   "studies.RDS",
