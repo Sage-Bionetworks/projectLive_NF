cc10dbc80aecf3d021fdbc4731b767c81ffd2d4e
jaybee84 <jinetab@gmail.com>
2021-05-16 12:06:18 -0700

    adding live versions of all RDS files

--- data-raw/files.R
+++ data-raw/files.R
@@ -1,30 +1,8 @@
 require(magrittr)
 devtools::load_all()
 syn <- create_synapse_login()
-<<<<<<< HEAD
 studies <- get_synapse_tbl(syn, "syn16787123") 
 
-# live ----
-
-files <- get_synapse_tbl(syn, "syn16858331") 
-
-files <- files %>% 
-=======
-files <- get_synapse_tbl(syn, "syn16858331")
-studies <- get_synapse_tbl(syn, "syn16787123")
-files <- files %>%
-  dplyr::rename("consortium" = "initiative") %>%
->>>>>>> master
-  dplyr::filter(.data$type == "file") %>% 
-  dplyr::mutate(fundingAgency = studies$fundingAgency[match(projectId, studies$studyId)]) 
-
-saveRDS(files, "files.RDS")
-store_file_in_synapse(
-  "files.RDS",
-  "syn22281727"
-)
-
-# develop ----
 dev_files <-
   projectlive.modules::get_synapse_tbl(
     syn,
@@ -67,10 +45,17 @@ dev_files <-
     by = c("projectId" = "studyId")
   )
   
-
+# develop ----
 saveRDS(dev_files, "files.RDS")
 store_file_in_synapse(
   "files.RDS",
   "syn24474593"
+)
+
+# live ----
+saveRDS(dev_files, "files.RDS")
+store_file_in_synapse(
+  "files.RDS",
+  "syn22281727"
 )
 
--- data-raw/tools.R
+++ data-raw/tools.R
@@ -4,15 +4,14 @@ syn <- create_synapse_login()
 tools <- get_synapse_tbl(syn, "syn16859448")
 saveRDS(tools, "tools.RDS")
 
-# live ----
-store_file_in_synapse(
-  "tools.RDS",
-  "syn22281727"
-)
-
 # dev ----
 store_file_in_synapse(
   "tools.RDS",
   "syn24474593"
 )
 
+# live ----
+store_file_in_synapse(
+  "tools.RDS",
+  "syn22281727"
+)
