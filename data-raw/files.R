212addba3c79f032f4dfaaaf39f0d2733c5e74c3
Jineta Banerjee <jinetab@gmail.com>
2021-05-22 08:25:37 -0700

    Revert "Update synapse auth"

--- data-raw/files.R
+++ data-raw/files.R
@@ -3,6 +3,21 @@ devtools::load_all()
 syn <- create_synapse_login()
 studies <- get_synapse_tbl(syn, "syn16787123") 
 
+# live ----
+
+files <- get_synapse_tbl(syn, "syn16858331") 
+
+files <- files %>% 
+  dplyr::filter(.data$type == "file") %>% 
+  dplyr::mutate(fundingAgency = studies$fundingAgency[match(projectId, studies$studyId)]) 
+
+saveRDS(files, "files.RDS")
+store_file_in_synapse(
+  "files.RDS",
+  "syn22281727"
+)
+
+# develop ----
 dev_files <-
   projectlive.modules::get_synapse_tbl(
     syn,
@@ -45,17 +60,10 @@ dev_files <-
     by = c("projectId" = "studyId")
   )
   
-# develop ----
+
 saveRDS(dev_files, "files.RDS")
 store_file_in_synapse(
   "files.RDS",
   "syn24474593"
-)
-
-# live ----
-saveRDS(dev_files, "files.RDS")
-store_file_in_synapse(
-  "files.RDS",
-  "syn22281727"
 )
 
--- data-raw/studies.R
+++ data-raw/studies.R
@@ -1,18 +1,17 @@
 require(magrittr)
 devtools::load_all()
 syn <- create_synapse_login()
-studies <- get_synapse_tbl(syn, "syn16787123") 
-
+studies <- get_synapse_tbl(syn, "syn16787123")
 saveRDS(studies, "studies.RDS")
-
-# dev ----
-store_file_in_synapse(
-  "studies.RDS",
-  "syn24474593"
-)
 
 # live ----
 store_file_in_synapse(
   "studies.RDS",
   "syn22281727"
+)
+
+# dev ----
+store_file_in_synapse(
+  "studies.RDS",
+  "syn24474593"
 )
